f29cd6b208b8b88287b3b60c4c854faf59a28731
Luis <luiscvlh11@gmail.com>
2024-03-25 21:39:10 -0300

    test: :white_check_mark: implementa o teste do metodo get do adapter do banco de dados

--- adapters/db/product_test.go
+++ adapters/db/product_test.go
@@ -1,11 +1,17 @@
 package db_test
 
-import "database/sql"
+import (
+	"appproduct/adapters/db"
+	"database/sql"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 var Db *sql.DB
 
 func setUp() {
-	Db, _ = sql.Open("sqlite3", ":memory")
+	Db, _ = sql.Open("sqlite3", ":memory:")
 	createTable(Db)
 	createProduct(Db)
 }
@@ -40,4 +46,17 @@ func createProduct(db *sql.DB) {
 	}
 
 	stmt.Exec()
+}
+
+func TestProductDb_Get(t *testing.T) {
+	setUp()
+	defer Db.Close()
+
+	productDb := db.NewProductDb(Db)
+	product, err := productDb.Get("abc")
+
+	require.Nil(t, err)
+	require.Equal(t, "Product ABC", product.GetName())
+	require.Equal(t, 100.0, product.GetPrice())
+	require.Equal(t, "DISABLED", product.GetStatus())
 }
