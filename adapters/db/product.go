b50d98fb78901b9aab188cd9755e7b184413cc4a
Luis <luiscvlh11@gmail.com>
2024-03-25 21:19:36 -0300

    feat: :sparkles: implementa o metodo get do adapter do banco de dados

---
+++ adapters/db/product.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"appproduct/application"
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type ProductDb struct {
+	db *sql.DB
+}
+
+func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
+	var product application.Product
+
+	stmt, err := p.db.Prepare("SELECT id, name, price, status FROM products where id=?")
+	if err != nil {
+		return nil, err
+	}
+
+	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price, &product.Status)
+	if err != nil {
+		return nil, err
+	}
+
+	return &product, nil
+}
